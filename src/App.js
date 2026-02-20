0566e8fc3626b74abe7e9ecc2a7daa69c8b1c312
Laura-html <lrichards023@gmail.com>
2026-02-20 04:45:42 +0000

    Added github link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import react from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was coding by Laura and is open-sources on{" "}
+        <a
+          href="https://github.com/Laura-html/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
