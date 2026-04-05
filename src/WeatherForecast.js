2e3747414cb2cb6de268f798d913902c2499911b
Laura-html <lrichards023@gmail.com>
2026-04-05 06:07:14 +0100

    updated weatherforecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -30,17 +30,9 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
         </div>
       </div>
     );
