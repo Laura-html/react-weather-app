74ffbb62839cba4b448004abba4b8bf72fe59a89
Laura-html <lrichards023@gmail.com>
2026-04-05 05:30:08 +0100

    updated icon for shecodes api

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <WeatherIcon code={props.data.icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
